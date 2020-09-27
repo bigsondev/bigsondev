808bdbfb13d026fe8acd4b215cddf59c95ba17b1
Adrian Bigaj <adrian.bigaj@clxcommunications.com>
2020-09-27 19:03:14 +0200

    Add gatsby SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,20 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from 'react';
+import { Layout } from 'antd';
+
+import { CookieBanner, Header, GlobalStyles, ContentHolder } from '~components';
+import { getItem } from '~utils';
+
+export const wrapPageElement = ({ element }) => (
+  <>
+    <GlobalStyles />
+    <Layout>
+      <Header />
+      <ContentHolder>{element}</ContentHolder>
+    </Layout>
+    {getItem('cookiesAcknowledged') === false && <CookieBanner />}
+  </>
+);
