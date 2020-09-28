ce1f39b7d88d9520db7c82edf4cd207034fbbd10
Adrian Bigaj <adrian.bigaj@clxcommunications.com>
2020-09-28 23:56:27 +0200

    Revert offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,6 +87,6 @@ module.exports = {
     'gatsby-plugin-webpack-bundle-analyser-v2',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
   ],
 };
