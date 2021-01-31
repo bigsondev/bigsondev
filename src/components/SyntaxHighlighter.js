1fbc8783c668f3cba7433a99eb9196077c06d790
Adrian Bigaj <adrian.bigaj@bigsondev.com>
2021-01-31 11:51:13 +0100

    Trick Highligter

--- src/components/SyntaxHighlighter.js
+++ src/components/SyntaxHighlighter.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Controlled as CodeMirror } from 'react-codemirror2';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
 
 const Holder = styled.div({
   position: 'relative',
+  pointerEvents: 'none',
   marginBottom: '1rem',
   '& code': {
     backgroundColor: '#ecd9c26e',
@@ -22,12 +23,46 @@ const CodeMirrorHolder = styled(CodeMirror)(({ theme, title }) => ({
   '& .CodeMirror-cursor': { display: 'none !important' },
 }));
 
+const isMobile = () => {
+  const toMatch = [
+    /Android/i,
+    /webOS/i,
+    /iPhone/i,
+    /iPad/i,
+    /iPod/i,
+    /BlackBerry/i,
+    /Windows Phone/i,
+  ];
+
+  if (window !== 'undefined') {
+    return toMatch.some((toMatchItem) => {
+      return navigator.userAgent.match(toMatchItem);
+    });
+  }
+
+  return false;
+};
+
 export const SyntaxHighlighter = ({
   language = 'javascript',
   title = '',
   children,
+  selection,
   ...props
 }) => {
+  const mobile = isMobile();
+  // Add dummy line to trick Highlighter on mobile...
+  const selectedLines = selection
+    ? mobile
+      ? [
+          {
+            anchor: { ch: 0, line: 0 },
+            head: { line: 0 },
+          },
+          ...selection.ranges,
+        ]
+      : selection.ranges
+    : [];
   return (
     <Holder>
       <CodeMirrorHolder
@@ -44,8 +79,11 @@ export const SyntaxHighlighter = ({
           matchTags: false,
           autoCloseTags: false,
           showHint: false,
-          readOnly: true,
+          readOnly: 'nocursor',
           lineNumbers: false,
+        }}
+        selection={{
+          ranges: selectedLines,
         }}
         {...props}
       />
